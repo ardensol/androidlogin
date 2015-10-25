c8b92d4597885750ac9762fc428c0dab7f7863a6
carlos <csolorio05@gmail.com>
2015-10-25 19:51:01 -0400

    frist commit

---
+++ app/src/main/java/com/carlos/androidlogin/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.carlos.androidlogin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected EditText mEmail;
+    protected EditText mPassword;
+    protected Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mEmail = (EditText) findViewById(R.id.loginEmailText);
+        mPassword = (EditText) findViewById(R.id.loginPassword);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                email = email.trim();
+                password = password.trim();
+
+
+
+            }
+        });
+
+    }
+
+}
